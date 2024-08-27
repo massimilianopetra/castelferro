1f765799ebd6df0b1f99c9a31cbd2646b200e57f
Massimiliano Petra <massimiliano.petra@gmail.com>
2024-08-27 21:52:44 +0200

    Correzione menu e funzione seed per inserimento db

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -53,233 +53,233 @@ const menu = [
   {
     id: 1,
     piatto: 'Pane e Coperto',
-    prezzo: '2,5',
-    cucina: 'all'
-    disponibile: "Y"
+    prezzo: 2.50,
+    cucina: 'All',
+    disponibile: 'Y'
   },
   {
     id: 2,
     piatto: 'Bresaola equina e rucola',
-    prezzo: '5,0',
-    cucina: 'antipasti'
+    prezzo: 5.00,
+    cucina: 'Antipasti',
     disponibile: "Y"
   },
   {
     id: 3,
     piatto: 'Antipasto equino',
-    prezzo: '5,0',
-    cucina: 'antipasti'
+    prezzo: 5.00,
+    cucina: 'Antipasti',
     disponibile: "Y"
   },
   {
     id: 4,
     piatto: 'Prosciutto crudo e melone',
-    prezzo: '5,0',
-    cucina: 'antipasti'
+    prezzo: 5.00,
+    cucina: 'Antipasti',
     disponibile: "Y"
   },
   {
     id: 5,
     piatto: 'Robiola biologica 100% latte di capra',
-    prezzo: '3,5',
-    cucina: 'antipasti'
+    prezzo: 3.50,
+    cucina: 'Antipasti',
     disponibile: "Y"
   },
   {
     id: 6,
     piatto: 'Caprino alle erbe',
-    prezzo: '3,5',
-    cucina: 'antipasti'
+    prezzo: 3.50,
+    cucina: 'Antipasti',
     disponibile: "Y"
   },
   {
     id: 7,
     piatto: 'Agnolotti al sugo',
-    prezzo: '7,5',
-    cucina: 'primi'
+    prezzo: 7.50,
+    cucina: 'Primi',
     disponibile: "Y"
   },
   {
     id: 8,
     piatto: 'Agnolotti al vino',
-    prezzo: '6,0',
-    cucina: 'primi'
+    prezzo: 6.00,
+    cucina: 'Primi',
     disponibile: "Y"
   },
   {
     id: 9,
     piatto: 'Agnolotti al butto e/o formaggio',
-    prezzo: '6,0',
-    cucina: 'primi'
+    prezzo: 6.00,
+    cucina: 'Primi',
     disponibile: "Y"
   },
   {
     id: 10,
     piatto: 'Polenta e tapulone',
-    prezzo: '8,0',
-    cucina: 'primi'
+    prezzo: 8.00,
+    cucina: 'Primi',
     disponibile: "Y"
   },
   {
     id: 11,
     piatto: 'Arrosto d`asino',
-    prezzo: '9,0',
-    cucina: 'secondi'
+    prezzo: 9.00,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 12,
     piatto: 'Stracotto d`asino',
-    prezzo: '9,0',
-    cucina: 'secondi'
+    prezzo: 9.00,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 13,
     piatto: 'Salamini al cartoccio',
-    prezzo: '7,0',
-    cucina: 'secondi'
+    prezzo: 7.00,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 14,
     piatto: 'Braciola di maiale',
-    prezzo: '6,0',
-    cucina: 'secondi'
+    prezzo: 6.00,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 15,
     piatto: 'Patatine fritte',
-    prezzo: '3,0',
-    cucina: 'secondi'
+    prezzo: 3.00,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 16,
     piatto: 'Peperonata',
-    prezzo: '3,5',
-    cucina: 'secondi'
+    prezzo: 3.50,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 17,
     piatto: 'Insalasta verde',
-    prezzo: '3,0',
-    cucina: 'secondi'
+    prezzo: 3.00,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 18,
     piatto: 'Fagioli alla texana',
-    prezzo: '3,5',
-    cucina: 'secondi'
+    prezzo: 3.50,
+    cucina: 'Secondi',
     disponibile: "Y"
   },
   {
     id: 19,
     piatto: 'Melone a fette',
-    prezzo: '2,0',
-    cucina: 'antipasti'
+    prezzo: 2.00,
+    cucina: 'Antipasti',
     disponibile: "Y"
   },
   {
     id: 20,
     piatto: 'Torta di nocciole',
-    prezzo: '3,5',
-    cucina: 'dolci'
+    prezzo: 3.50,
+    cucina: 'Dolci',
     disponibile: "Y"
   },
   {
     id: 21,
     piatto: 'Crostata',
-    prezzo: '3,5',
-    cucina: 'dolci'
+    prezzo: 3.50,
+    cucina: 'Dolci',
     disponibile: "Y"
   },
   {
     id: 22,
     piatto: 'Salame dolce al cioccolato',
-    prezzo: '3,5',
-    cucina: 'dolci'
+    prezzo: 3.50,
+    cucina: 'Dolci',
     disponibile: "Y"
   },
   {
     id: 23,
     piatto: 'Salame dolce al marmellata',
-    prezzo: '3,5',
-    cucina: 'dolci'
+    prezzo: 3.50,
+    cucina: 'Dolci',
     disponibile: "Y"
   },
   {
     id: 24,
     piatto: 'Pastine della nonna',
-    prezzo: '2,5',
-    cucina: 'dolci'
+    prezzo: 2.50,
+    cucina: 'Dolci',
     disponibile: "Y"
   },
   {
     id: 25,
     piatto: 'Dolcetto d`Ovada DOC',
-    prezzo: '7,0',
-    cucina: 'bevande'
+    prezzo: 7.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 26,
     piatto: 'Barbera del monferrato DOC',
-    prezzo: '7,0',
-    cucina: 'bevande'
+    prezzo: 7.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 27,
     piatto: 'Bonarda',
-    prezzo: '7,0',
-    cucina: 'bevande'
+    prezzo: 7.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 28,
     piatto: 'Pinot Bianco DOC',
-    prezzo: '7,0',
-    cucina: 'bevande'
+    prezzo: 7.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 29,
     piatto: 'Rugiada Chardonnay DOC',
-    prezzo: '7,0',
-    cucina: 'bevande'
+    prezzo: 7.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 30,
     piatto: 'Moscato Piemonte',
-    prezzo: '7,0',
-    cucina: 'bevande'
+    prezzo: 7.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 31,
     piatto: 'Birra artigianale 0,4lt',
-    prezzo: '4,0',
-    cucina: 'birre'
+    prezzo: 4.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 32,
     piatto: 'Acqua Minerale naturale 0,5lt',
-    prezzo: '1,0',
-    cucina: 'bevande'
+    prezzo: 1.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
   {
     id: 33,
     piatto: 'Acqua Minerale frizzante 0,5lt',
-    prezzo: '1,0',
-    cucina: 'bevande'
+    prezzo: 1.00,
+    cucina: 'Bevande',
     disponibile: "Y"
   },
 ];
-export { users };
+export { menu };
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { users } from '../lib/placeholder-data';
+import { menu } from '../lib/placeholder-data';
 
 const client = await db.connect();
 
@@ -15,9 +16,12 @@ async function seedUsers() {
      );
    `;
 
+  console.log(`CREATD TABLE users`);
+
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
+      console.log(`VALUES ${user.id}, ${user.name}, ${user.email}, ${hashedPassword}`);
       return client.sql`
            INSERT INTO users (id, name, email, password)
            VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
@@ -29,11 +33,40 @@ async function seedUsers() {
   return insertedUsers;
 }
 
+async function seedMenu() {
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS menus (
+       id INTEGER PRIMARY KEY,
+       piatto VARCHAR(255) UNIQUE,
+       prezzo REAL,
+       cucina VARCHAR(255),
+       disponibile VARCHAR(3)
+     );
+   `;
+
+   console.log(`CREATD TABLE menus`);
+
+
+  const insertedMenu = await Promise.all(
+    menu.map(async (item) => {
+      console.log(`VALUES ${item.id}, ${item.piatto}, ${item.prezzo}, ${item.cucina},${item.disponibile}`);
+      return client.sql`
+           INSERT INTO menus (id, piatto, prezzo, cucina,disponibile)
+           VALUES (${item.id}, ${item.piatto}, ${item.prezzo}, ${item.cucina},${item.disponibile})
+           ON CONFLICT (id) DO NOTHING;
+        `;
+    }),
+  );
+
+  return insertedMenu;
+}
+
 export async function GET() {
 
   try {
     await client.sql`BEGIN`;
     await seedUsers();
+    await seedMenu();
     await client.sql`COMMIT`;
 
     return Response.json({ message: 'Database seeded successfully' });
