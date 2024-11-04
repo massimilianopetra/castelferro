3c40328ea41d9ab84924f083795dc551997cce33
Bruno Bottiero <bruno.bottiero@gmail.com>
2024-11-04 23:20:47 +0100

    tolto test schermo e modificato lg: in md:

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,9 @@ export default  function Layout({ children }: Props) {
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
           <SideNav />
+          
         </div>
+        <div className="bg-red-500 sm:bg-green-500 md:bg-blue-500 lg:bg-pink-500 xl:bg-teal-500">sss</div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
     </NextAuthProvider>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
                         Home
                     </p>
                 </div>
-
+                <div className="bg-red-500 sm:bg-green-500 md:bg-blue-500 lg:bg-pink-500 xl:bg-teal-500"></div>
                 <DashboardLinks/>
 
             </div>
