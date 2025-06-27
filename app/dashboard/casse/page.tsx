f58c712572ee96baedd4f1fafaff0a64ae5adbde
Bruno Bottiero <bruno.bottiero@gmail.com>
2025-06-28 00:44:42 +0200

    migliorata tutta GUI per smarphone cucine

--- app/dashboard/casse/page.tsx
+++ app/dashboard/casse/page.tsx
@@ -53,12 +53,12 @@ export default function Page() {
 
     const handleButtonClickCaricaAsporto = async () => {
         const ultconto = await getContoPiuAlto();
-        if (ultconto){
+        if (ultconto) {
             var uc = Number(ultconto);
             if (uc < 8999)
-                uc = 9000 
-        carica(uc+1);
-    }
+                uc = 9000
+            carica(uc + 1);
+        }
     };
     const handleButtonClickCaricaConto1 = async () => {
         carica(1);
@@ -84,19 +84,22 @@ export default function Page() {
                 </main>
             )
         else
-            return (<main>
+            return (
+            <main>
                 <div className="container">
                     <header className="top-section">
                         <div className="sez-sx">
-                            <div className="p-1 mb-1 font-extralight border-4 border-blue-600 shadow-2xl bg-blue-200 text-end rounded-full">
-                                <ul className="flex rounded-full">
-                                    <li className="flex-1 mr-2font-bold py-4 rounded-full">
-                                        <a className="text-center block text-white font-extraligh text-2xl md:text-5xl">
+                            <div className="p-1 mb-1 font-extralight border-4 border-blue-600 shadow-2xl bg-blue-200 text-end rounded-full" style={{ borderRadius: '9999px' }}>
+                                <ul className="flex rounded-full" style={{ borderRadius: '9999px' }}>
+                                    <li className="flex-1 mr-2font-bold py-4 ">
+                                        <a className="text-center block text-blue-700 font-extraligh text-2xl md:text-5xl">
                                             Casse
                                         </a>
-                                        <div className="text-xs text-center text-white ">SAGRA:  {sagra.stato}&nbsp;&nbsp;{(sagra.stato == 'CHIUSA') ? "" : "(" + sagra.giornata + ")"}</div>
+                                        <div className="text-xs text-center text-blue-700 ">SAGRA:
+                                            <span className="text-xs text-center text-blue-800 ">{sagra.stato}&nbsp;{(sagra.stato == 'CHIUSA') ? "" : "(" + sagra.giornata + ")"}</span>
+                                        </div>
                                     </li>
-                                    <li className="text-right flex-1 mr-2 text-5xl  text-white font-bold py-4">
+                                    <li className="text-right flex-1 mr-2 text-5xl  text-white font-bold py-4 rounded-full " style={{ borderRadius: '9999px' }}>
                                         <div className='text-center text-emerald-600'>
                                             <TextField
                                                 autoFocus
@@ -116,12 +119,12 @@ export default function Page() {
                                         </div>
 
                                     </li>
-                                    <li className="text-left flex-1 mr-2 text-5xl font-bold py-4 rounded-full">
+                                    <li className="text-left flex-1 mr-2 text-5xl font-bold py-4 ">
                                         <ButtonGroup sx={{ display: { xs: 'none', md: 'block' } }}>
-                                            <Button className="rounded-full" variant="contained" onClick={handleButtonClickCarica} style={{ borderRadius: '9999px' }}>Carica Foglietto</Button>
+                                            <Button  variant="contained" onClick={handleButtonClickCarica} style={{ borderRadius: '9999px' }}>Carica Foglietto</Button>
                                         </ButtonGroup>
                                         <ButtonGroup sx={{ display: { xs: 'block', md: 'none' } }}>
-                                            <Button size="small" className="rounded-full" variant="contained" onClick={handleButtonClickCarica} style={{ borderRadius: '9999px' }}>Carica Foglietto</Button>
+                                            <Button size="small" variant="contained" onClick={handleButtonClickCarica} style={{ borderRadius: '9999px' }}>Carica Foglietto</Button>
                                         </ButtonGroup>
 
                                     </li>
@@ -129,21 +132,21 @@ export default function Page() {
                             </div>
                             <div className="text-base md:text-xl" style={{ display: 'flex', alignItems: 'center', flexWrap: 'nowrap', justifyContent: 'flex-end' }}>
                                 <ButtonGroup sx={{ display: { xs: 'none', md: 'block' } }}>
-                                    <p>Ultimi ricercati &nbsp;
+                                    <p><span className="text-blue-800 ">Ultimi ricercati &nbsp;</span>
                                         {lastLog.map((row) => (
-                                        <>
-                                            <Button size="medium" className="rounded-full" variant="contained" onClick={() => { carica(row.foglietto) }} startIcon={<Filter1Icon />} style={{ borderRadius: '9999px' }}>{row.foglietto}</Button>
-                                             &nbsp;
-                                        </>
+                                            <>
+                                                <Button size="medium" variant="contained" onClick={() => { carica(row.foglietto) }} startIcon={<Filter1Icon />} style={{ borderRadius: '9999px' }}>{row.foglietto}</Button>
+                                                &nbsp;
+                                            </>
                                         ))}</p>
                                 </ButtonGroup>
                                 <ButtonGroup sx={{ display: { xs: 'block', md: 'none' } }}>
-                                    <p>Ultimi &nbsp;
+                                    <p><span className="text-blue-800 ">Ultimi  &nbsp;</span>
                                         {lastLog.map((row) => (
-                                         <>
-                                            <Button size="small" className="rounded-full" variant="contained" onClick={() => { carica(row.foglietto) }} startIcon={<Filter1Icon />} style={{ borderRadius: '9999px' }}>{row.foglietto}</Button>
-                                            &nbsp;
-                                        </>
+                                            <>
+                                                <Button size="small"  variant="contained" onClick={() => { carica(row.foglietto) }} startIcon={<Filter1Icon />} style={{ borderRadius: '9999px' }}>{row.foglietto}</Button>
+                                                &nbsp;
+                                            </>
                                         ))}</p>
                                 </ButtonGroup>
                             </div>
@@ -151,26 +154,20 @@ export default function Page() {
                         </div>
                         <div className="sez-dx" style={{ display: 'flex', flexWrap: 'nowrap', justifyContent: 'flex-end' }}>
                             <div className="xl:text-3xl xl:py-4 font-extralight text-end lg:text-base lg:py-1">
-                                <Button size="medium" className="font-semibold rounded-full" variant="outlined" onClick={handleButtonClickCaricaAsporto} style={{ borderRadius: '9999px' }}>Asporto</Button>
+                                <Button size="medium" className="font-semibold" variant="outlined" onClick={handleButtonClickCaricaAsporto} style={{ borderRadius: '9999px' }}>Asporto</Button>
                                 &nbsp;&nbsp;
-                                <Button size="medium" color="secondary" className="font-semibold rounded-full" variant="outlined" onClick={handleButtonClickCaricaConto1} style={{ borderRadius: '9999px' }}>Camerieri</Button>
+                                <Button size="medium" color="secondary" className="font-semibold " variant="outlined" onClick={handleButtonClickCaricaConto1} style={{ borderRadius: '9999px' }}>Camerieri</Button>
                             </div>
                             <br /><br /><br /><br /><br />
                         </div>
 
                     </header>
 
-                    <main className="middle-section">
-                        
-                    </main>
+                    <main className="middle-section"> </main>
 
                     <footer className="bottom-section">
-                        <div className="sez-sx-bassa">
-
-                        </div>
-                        <div className="sez-dx-bassa">
-
-                        </div>
+                        <div className="sez-sx-bassa"></div>
+                        <div className="sez-dx-bassa"></div>
                     </footer>
                 </div>
                 <Snackbar
@@ -181,71 +178,9 @@ export default function Page() {
                         "1 Inserisci un numero foglietto valido (minore di 8999)" :
                         "2 Hai inserito un numero riservato asporto (compreso tra 9000 e 9999)"
                     }
-                />                </main>)
-        /*
-     return (
-                <main>
-                    
-                    <div className="z-50 lg:fixed xl:fixed md:fixed p-1 mb-1 font-extralight border-4 border-blue-600 shadow-2xl bg-blue-200 text-end rounded-full">
-                        <ul className="flex rounded-full">
-                            <li className="flex-1 mr-2 text-5xl font-bold py-4 rounded-full">
-                                <a className="text-center block text-white font-extralight ">
-                                    Casse
-                                </a>
-                                <div className="text-xs text-center text-white ">SAGRA:  {sagra.stato}&nbsp;&nbsp;{(sagra.stato == 'CHIUSA') ? "" : "(" + sagra.giornata + ")"}</div>
-                            </li>
-                            <li className="text-right flex-1 mr-2 text-5xl  text-white font-bold py-4">
-                                <a>
-                                    <div className='text-center text-emerald-600'>
-                                        <TextField
-                                            autoFocus
-                                            className="p-2"
-                                            label="Numero Foglietto"
-                                            variant="outlined"
-                                            value={numero}
-                                            onChange={handleInputChange}
-                                            sx={{
-                                                input: {
-                                                    textAlign: 'right', // Allinea il testo a destra
-                                                },
-                                            }}
-                                            type="number"
-                                        />
-                                    </div>
-                                </a>
-                            </li>
-                            <li className="text-left flex-1 mr-2 text-5xl font-bold py-4 rounded-full">
-                                <Button className="rounded-full" variant="contained" onClick={handleButtonClickCarica}>Carica Foglietto</Button>
-                            </li>
-                        </ul>
-                    </div>
+                />
+            </main>)
 
-                    <div className="z-0 xl:text-2xl  xl:py-4 font-extralight xl:text-end md:text-base md:py-2 md:text-center">
-                        <p>Ultimi ricercati: &nbsp;
-                            {lastLog.map((row) => (
-                                <>
-                                    <Button size="medium" className="rounded-full" variant="contained" onClick={() => { carica(row.foglietto) }} startIcon={<Filter1Icon />}>{row.foglietto}</Button>
-                                    &nbsp;&nbsp;
-                                </>
-                            ))}
-                       <div className="z-0 xl:text-3xl xl:py-4 font-extralight text-end lg:text-base lg:py-1">
-            <Button size="medium" className="font-semibold rounded-full" variant="outlined"  onClick={handleButtonClickCaricaAsporto}>Asporto</Button>
-            &nbsp;&nbsp;
-            <Button size="medium" color="secondary" className="font-semibold rounded-full" variant="outlined"  onClick={handleButtonClickCaricaConto1}>Camerieri</Button>
-          </div> </p>
-                    </div>
-                    <Snackbar
-                            open={openSnackbar}
-                            autoHideDuration={6001}
-                            onClose={handleClose}
-                            message={(+numero) > 9999 ?
-                                "1 Inserisci un numero foglietto valido (minore di 8999)":
-                                "2 Hai inserito un numero riservato asporto (compreso tra 9000 e 9999)"
-                            }
-                        />
-                </main>
-
-            )*/
     else
         return (
             <main>
@@ -257,7 +192,6 @@ export default function Page() {
                     </div>
                 </div>
             </main>
-
         )
 
 }
